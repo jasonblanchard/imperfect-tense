00e173a578fd5ae6d6422ef47c63728e4d14f8e7
Jason Blanchard <jas.blanchard@gmail.com>
2014-01-26 14:23:54 -0500

    Started adding links to notes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,34 @@
+$(document).ready(function() {
+	
+	// Add ids to interjection nodes
+	$('.interjection-text').find('ol li').each(function(index, vaue) {
+		$(this).attr('id', "intj-" + (index + 1))
+	});
 
+	
+	$('sup').each(function(index, value) {
+		
+		var text = $(this).text();
+				
+		if ( !isNaN(parseInt(text)) ) {
+			$(this).attr('id', 'goto-intj-' + text)
+			$(this).data('goto', 'intj-' + text)
+		}
+		
+				
+		$(this).html("<a href='#'>" + text + "</a>");
+		
+		// Add click events and 
+		// check context of sup to see which note to link to
+		
+		$(this).on('click', function(e) {
+			e.preventDefault();
+			
+			var goToId = "#" + $(this).data('goto');
+			
+			console.log(goToId);
+			
+			$(goToId)[0].scrollIntoView();
+		});
+	});
+});
